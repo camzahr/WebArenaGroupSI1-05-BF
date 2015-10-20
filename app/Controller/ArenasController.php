a6c0a0ff34ef9f286954cbd921ec3f5bb88fc4ca
camzahr <camiller@ece.fr>
2015-10-20 14:08:29 +0200

    1st Commit

---
+++ app/Controller/ArenasController.php
@@ -0,0 +1,64 @@
+<?php 
+
+App::uses('AppController', 'Controller');
+
+/**
+ * Main controller of our small application
+ *
+ * @author ...
+ */
+class ArenasController extends AppController
+{
+
+    /**
+     * index method : first page
+     *
+     * @return void
+     */
+    public function index()
+    {
+       $this->set('myname', "Jérémy Camilleri");
+    }
+    
+    /**
+     * index method : login
+     *
+     * @return void
+     */
+    public function login()
+    {
+        
+    }
+    
+    /**
+     * index method : first page
+     *
+     * @return void
+     */
+    public function fighter()
+    {
+        
+    }
+    
+    /**
+     * index method : first page
+     *
+     * @return void
+     */
+    public function sight()
+    {
+        
+    }
+    
+    /**
+     * index method : first page
+     *
+     * @return void
+     */
+    public function diary()
+    {
+        
+    }
+
+}
+?>
