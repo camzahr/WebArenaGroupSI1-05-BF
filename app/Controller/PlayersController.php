d4feda1d2729cf1805b91037d5c30960b6e858ac
camzahr <camiller@ece.fr>
2015-11-08 11:48:48 +0100

    Merge branch 'master' into Test
    Conflicts:
    	app/nbproject/private/private.xml

---
+++ app/Controller/PlayersController.php
@@ -0,0 +1,43 @@
+<?php 
+
+App::uses('AppController', 'Controller');
+
+/**
+ * Main controller of our small application
+ *
+ * @author ...
+ */
+class PlayersController extends AppController
+{
+    public $uses = array('Player');
+
+    /**
+     * subscribe method : first page
+     *
+     * @return void
+     */
+    public function subscribe()
+    {
+        if ($this->request->is('post'))       
+        {            
+            pr($this->request->data);        
+        }
+        
+        //Si c'est une action de subscribe
+        if($this->request->data('Playersubscribe'))
+        {
+            $this->Player->subscribe($this->request->data['Playersubscribe']);
+        } 
+    }
+    
+    /**
+     * display method : first page
+     *
+     * @return void
+     */
+    public function display()
+    {
+        $this->set('raw',$this->Player->findById('545f827c-576c-4dc5-ab6d-27c33186dc3e'));
+    }
+    
+}
