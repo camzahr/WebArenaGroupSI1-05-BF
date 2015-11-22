bec593aabb331a265b90e915dee052f742fc09b2
jb <jb.gesnel@hotmail.fr>
2015-11-22 15:39:10 +0100

    db

--- app/Config/database.php
+++ app/Config/database.php
@@ -71,7 +71,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'root',
+		'password' => '',
 		'database' => 'ece_webarena',
 		'prefix' => '',
 		//'encoding' => 'utf8',
